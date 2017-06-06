7d44c85d449827e90d67e7ea5dfac091a45c6cb3
openja moses <openjamosesopm@gmail.com>
2017-06-06 19:18:41 +0300

    Update Main.java

--- JSONGit/src/Opm_Package/Main.java
+++ JSONGit/src/Opm_Package/Main.java
@@ -1,5 +1,11 @@
 package Opm_Package;
 
+
+import static Opm_Package.TestDate.filePath;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,16 +13,23 @@ package Opm_Package;
  */
 
 
-
-
 /**
  *
  * @author john
  */
 public class Main {
-
-    static String  pname = "Integreight/1Sheeld-Android-App";
-       
+    
+    Main() throws Exception{
+        getReposName();
+    }
+    
+    
+    private static final String filePath = "/Users/john/Desktop/PROJECTS/JSON WORK/Repo_Names.xlsx";
+    
+                             
+    private static final String[] toks = {
+                                          };
+            
     /**
      * @param args the command line arguments
      */
@@ -24,8 +37,34 @@ public class Main {
        public static void main(String[] args) throws Exception {
         // TODO code application logic here
         System.out.println("Please wait ...");
-        new ReadCommits().readCommitsNow(pname);
-    }
-
-    
+        new Main(); 
+    }  
+       
+       public void getReposName() throws Exception{
+            OpenFileName fileName = new OpenFileName();
+            List<String> list = fileName.readReposNames(filePath);
+            List<String> sheetsList = new ArrayList<>();
+            for (int i=0; i<list.size(); i++){
+                 String[] splits = list.get(i).split("/");
+                 sheetsList.add(splits[0]);
+                 //System.out.println("Projects: "+list.get(i)+", \t \t Sheets: "+sheetsList.get(i));
+           }
+            // To convert List in to array of Strings..
+            String[] repos_array = new String[list.size()];
+            String[] sheet_array = new String[sheetsList.size()];
+            
+            repos_array = list.toArray(repos_array);
+            sheet_array = sheetsList.toArray(sheet_array);
+            XSSFSheet[] sheet = new XSSFSheet[repos_array.length];
+            int count = 0;
+            for(int i=1; i<repos_array.length; i++){
+                
+           // if(i > 0){
+                ReadCommits readCommit =  new ReadCommits();
+                readCommit.readCommitsNow(repos_array[i],toks,sheet_array[i],count,sheet);
+            //}
+                count ++;
+           } 
+        
+        }
 }
